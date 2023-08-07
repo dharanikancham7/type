a788cec7f207cbf37c2ce75fbbfd0acc37e5b2c4
SailendraRapeti <sailendrarapeti777@gmail.com>
2023-08-07 19:09:01 +0530

    types

---
+++ myapp/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let first : string="sai"
+  console.log(first)
+  return (
+    <div>
+      <h1>type script</h1>
+    
+    </div>
+  );
+}
+
+export default App;
